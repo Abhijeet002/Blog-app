e7eef1065fe6ce7cb011fab6bd62939a3eb01691
Abhijeet <abhijeetsachan.088@gmail.com>
2025-05-27 02:52:08 +0530

    add base url for posts

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,24 @@
-import path from "path"
-import { fileURLToPath } from "url"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-      "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./src")
-    },
-    server: {
+    "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./src"),
+  },
+  server: {
     proxy: {
       // Any request starting with /api or /auth will be proxied
-      '/auth': {
-        target: 'http://localhost:5000',
+      "/auth": {
+        target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
       },
+      "/posts": "http://localhost:5000",
     },
   },
-    
-  }
-)
+});
